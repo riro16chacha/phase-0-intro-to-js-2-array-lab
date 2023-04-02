12f4c9313da21e283c9aec92477bc26a91fd838f
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2023-04-02 14:58:05 +0300

    arrays lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+function  destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function  appendCat(name){
+    const copyOfCats = [...cats,"Broom"]
+    return copyOfCats;
+}
+function prependCat(name){
+    const catsnew = ["Arnold",...cats]
+    return catsnew;
+}
+function removeLastCat(){
+    const copyofnewcats = cats.slice(0,-1)
+    return copyofnewcats;
+}
+function removeFirstCat(){
+    const anothercopyofcats = cats.slice(1);
+    return anothercopyofcats;
+}
